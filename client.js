1a89ce601428fa66f6068974c8d1e3694ccd10d6
pablo lopez <pablo_98_18@hotmail.com>
2024-01-27 21:58:41 -0300

    primer commit ok

---
+++ client.js
@@ -0,0 +1,88 @@
+import { Client } from "discord.js"
+import { AudioPlayer, joinVoiceChannel, createAudioPlayer, createAudioResource } from "@discordjs/voice"
+import "dotenv/config"
+
+import path from "path"
+
+import { CreateVoice, GetVoice, GetAllVoices } from "./src/modules/VoiceClient.js"
+
+import * as url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const client = new Client({
+	intents: [
+		3276799
+	]
+})
+
+client.on("ready", () => {
+	console.log("Bot en linea broder")
+})
+
+client.on("messageCreate", (message) => {
+	try {
+		const vc = message.member.voice.channel
+		const msg = message.content
+
+		if (msg.substring(0, 1) == "'" && vc && vc.joinable && message.member.user.bot == false) {
+			let messageContent = msg.substring(2).trim()
+			const split = messageContent.split(" ")
+
+			if (split[0] && split[0].toLowerCase() == "voices") { // Comando: "' voices"
+				const Voices = GetAllVoices()
+				let Text = ""
+
+				Voices.forEach(e => {
+					Text = Text + `${e.Id} (${e.LanguageCode})\n`
+				})
+
+				message.reply(Text)
+			} else if (split[0]) {
+
+				let UsedVoice = "Miguel" // Voz por defecto
+
+				const Voice = GetVoice(split[0])
+				if (Voice) {
+					UsedVoice = Voice.Id
+					split.shift()
+					messageContent = split.join(" ")
+				}
+
+				if (messageContent.length == 0) return
+
+				const connection = joinVoiceChannel({
+					channelId: vc.id,
+					guildId: vc.guildId,
+					adapterCreator: vc.guild.voiceAdapterCreator
+				})
+
+				const voiceBuffer = CreateVoice(messageContent, UsedVoice, `${message.guildId}`)
+				voiceBuffer.then(AudioBuffer => {
+					const player = createAudioPlayer()
+					const parte = path.join(__dirname, `/src/audio/${message.guildId}.mp3`)
+					const resource = createAudioResource(parte, { inlineVolume: true })
+					resource.volume.setVolume(2)
+
+					connection.subscribe(player)
+					player.play(resource)
+				})
+				.catch(err => {
+					message.reply("Error al procesar la solicitud en AWS")
+				})
+
+				if (messageContent.toLowerCase() == "pene"){
+					message.reply("comes")
+				}
+			}
+		} else {
+		}
+	} catch (err) {
+		console.log("Hubo un error en la deteccion de mensaje")
+		console.log(err)
+	}
+
+})
+
+client.login(process.env.DISCORD_LOGINTOKEN)
---
+++ src/modules/VoiceClient.js
@@ -0,0 +1,72 @@
+import AWS from "aws-sdk"
+import fs from "fs"
+import { URL } from "url"
+import "dotenv/config"
+
+const Polly = new AWS.Polly({
+	region: "eu-central-1"
+})
+
+let Voices = []
+
+const UpdateVoicesList = async () => {
+	Polly.describeVoices((err, vc) => {
+		if (!err) {
+			Voices = vc.Voices
+		}
+	})
+}
+
+const GetAllVoices = () => {
+	return Voices
+}
+
+const GetVoice = (Voice) => {
+	const Finded = Voices.find(v => {
+		if (v.Id.toLowerCase() === Voice.toLowerCase() || v.Name.toLowerCase() === Voice.toLowerCase()) {
+			return true
+		}
+	})
+	return Finded
+}
+
+const CreateVoice = async (Text, Voice = "miguel", AudioName) => {
+	try {
+		const promesa = new Promise((resolve, reject) => {
+
+			const VoiceConfig = GetVoice(Voice)
+			const tts_settings = {
+				Engine: "standard",
+				LanguageCode: VoiceConfig.LanguageCode,
+				OutputFormat: "mp3",
+				Text: Text,
+				TextType: "text",
+				VoiceId: VoiceConfig.Name
+			}
+
+			Polly.synthesizeSpeech(tts_settings, (err, data) => {
+				if (err) {
+					reject("Error al sintetizar")
+				}
+				if (!err && data.AudioStream) {
+					fs.writeFile(`./src/audio/${AudioName}.mp3`, data.AudioStream, "base64", (err) => {
+						if (err) {
+							console.log("Erro al guardar audio")
+							reject("Error al guardar el audio")
+						} else {
+							resolve(data.AudioStream)
+						}
+					})
+				}
+			})
+		})
+		return promesa
+	} catch (err) {
+		return 
+	}
+	
+}
+
+UpdateVoicesList()
+
+export { CreateVoice, GetVoice, GetAllVoices }
