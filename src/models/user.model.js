3e7394aec8d72a5ebb4135b9f1aab33ecfe495b3
pablo lopez <pablo_98_18@hotmail.com>
2024-02-08 21:05:30 -0300

    glinda defecto

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,8 +5,8 @@ const schema = new Schema({
 	voice: { type: String, required: false, default: "miguel"},
 	premium: { type: Boolean, required: false, default: false},
 	customvoice: {
-		selectedName: { type: String, required: false, default: "ethan"},
-		selectedId: { type: String, required: false, default: "g5CIjZEefAph4nQFvHAz"},
+		selectedName: { type: String, required: false, default: "glinda"},
+		selectedId: { type: String, required: false, default: "z9fAnlkpzviPz146aGWa"},
 		stability: { type: Number, required: false, default: 0.5},
 		similarity: { type: Number, required: false, default: 0.5},
 		exaggeration: { type: Number, required: false, default: 0}
